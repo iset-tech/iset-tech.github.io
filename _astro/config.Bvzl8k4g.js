90b35c49f19e29539f7403332273c4fee8aefc9c
root <root@jcyoo2.localdomain>
2025-01-04 10:30:51 +0900

    Deploy a page

---
+++ _astro/config.Bvzl8k4g.js
@@ -0,0 +1,1 @@
+const t={author:"Samuel Breznjak",title:"Minimal - Astro blog theme",description:"Minimalistic blog theme for astro",lang:"en",themes:{dark:"github-dark",light:"github-light"}};export{t as c};
