5c3cce6dcc6de00dff75635c37ddf851bb3ea0ab
root <root@jcyoo2.localdomain>
2025-01-05 11:02:32 +0900

    delete all

--- _astro/config.Bvzl8k4g.js
+++
@@ -1,1 +0,0 @@
-const t={author:"Samuel Breznjak",title:"Minimal - Astro blog theme",description:"Minimalistic blog theme for astro",lang:"en",themes:{dark:"github-dark",light:"github-light"}};export{t as c};
